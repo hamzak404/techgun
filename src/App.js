b0b344b8b00d95e7ea5abe7351f827b50a676ff4
Hamza Khan <hamzakhan032017@gmail.com>
2024-04-16 14:27:17 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
+import "./App.css";
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full",
+    timeStyle: "long",
+    timeZone: "Asia/Kolkata",
+  }).format(date);
+}
 function App() {
+  const [title, setTitle] = useState("asdf");
+  const [title2, setTitle2] = useState("asdfasdfakdjfjlkajsd");
+  const clikhandler = () => {
+    setTitle("new title");
+  };
+
+  const clikhandler2 = () => {
+    setTitle2(title2);
+  };
+  const changehandler = (event) => {
+    setTitle2(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>asdf </h1>
+      <span id="sed">{title}</span>
+      <br />
+      <span id="sed">{title2}</span>
+      <br />
+      <button onClick={clikhandler}>Click Me</button>
+      <button onClick={clikhandler}>Click Me</button>
+      <div className="alert alert-primary" role="alert">
+        <h4 className="alert-heading">Well done!</h4>
+        <p>Aww yeah, you successfully read this important alert message. </p>
+        <hr />
+        <p className="mb-0">
+          Whenever you need to, be sure to use margin utilities to keep things
+          nice and tidy.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <input
+        type="number"
+        step="0.2"
+        min="0.2"
+        name="ss"
+        id="ss"
+        value={title2}
+        onChange={changehandler}
+      />
+      <button onClick={clikhandler2}>Click Me</button>
     </div>
   );
 }
